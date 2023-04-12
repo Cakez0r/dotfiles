15fa9a4c40a5714eec374a074f16cba2479b6267
Lewis <lewis.ellington@gmail.com>
2023-04-12 16:33:51 +0100

    Latest

---
+++ .config/fish/config.fish
@@ -0,0 +1,25 @@
+function bind_bang
+    switch (commandline -t)[-1]
+        case "!"
+            commandline -t -- $history[1]
+            commandline -f repaint
+        case "*"
+            commandline -i !
+    end
+end
+
+function bind_dollar
+    switch (commandline -t)[-1]
+        case "!"
+            commandline -f backward-delete-char history-token-search-backward
+        case "*"
+            commandline -i '$'
+    end
+end
+
+function fish_user_key_bindings
+    bind ! bind_bang
+    bind '$' bind_dollar
+end
+
+alias config='/usr/bin/git --git-dir=/home/lewis/.cfg/ --work-tree=/home/lewis'
